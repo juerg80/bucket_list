d842887739f22cd3d20f4c82ef743450dbe7862a
juerg80epfl <juerg_mueller@vtxmail.ch>
2020-10-20 20:25:09 +0200

    added views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'styles/atoms'
+  get 'styles/molecules'
+  get 'styles/organisms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
