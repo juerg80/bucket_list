cdf73e24f3f6425c4a0cc4cbb24e81621bdf59b7
juerg80epfl <juerg_mueller@vtxmail.ch>
2020-10-25 15:24:32 +0100

    5.10: done

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,9 +3,13 @@ class IdeasController < ApplicationController
   def index
     @search_term = params[:q]
     logger.info("The search term is #{@search_term}")
+    @ideas = Idea.all
   end
 
   def create
+    new_idea = Idea.new
+    new_idea.title = params[:title]
+    new_idea.save!
     redirect_to ideas_index_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'ideas/index'
   get 'ideas/new'
   post 'ideas/create'
+  get '/account/ideas'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
