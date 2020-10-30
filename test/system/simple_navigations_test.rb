bc6e3f80aedecba403b88e7e5f74668a2636a998
juerg80epfl <juerg_mueller@vtxmail.ch>
2020-10-30 14:33:44 +0100

    6.6: done

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,11 +1,26 @@
 require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
-  test 'search term is displayed' do
+  # test 'search term is displayed' do
+  #   visit('/')
+  #   assert page.has_content?('Bucket List')
+  #   fill_in('q', with: 'Spain').send_keys(:enter)
+  #   assert has_content?('Spain')
+  #   assert current_url.include?('q=Spain')
+  # end
+
+  test 'style guide navigation' do
     visit('/')
-    assert page.has_content?('Bucket List')
-    fill_in('q', with: 'Spain').send_keys(:enter)
-    assert has_content?('Spain')
-    assert current_url.include?('q=Spain')
+    click_on('Style Guide')
+    assert has_content?('Atoms')
+    assert has_content?('Molecules')
+    assert has_content?('Organisms')
+    click_on('Molecules')
+    assert has_content?('Card')
+    click_on('Organisms', match: :first)
+    assert has_content?('Create Goal')
+    click_on('My Bucket List', match: :first)
+    assert has_content?('Some ideas')
   end
+
 end
