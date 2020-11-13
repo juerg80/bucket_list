8cb900dded416387ac3ba91a7a08d92e7a3f5031
juerg80epfl <juerg_mueller@vtxmail.ch>
2020-11-13 14:08:03 +0100

    Task: 9.4 done

---
+++ test/system/idea_comments_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class IdeaCommentsTest < ApplicationSystemTestCase
+
+  test 'adding a Comment to an Idea' do
+    my_idea = Idea.new
+    my_idea.title = "my title"
+    my_idea.done_count = 99
+    my_idea.photo_url = "turtle.jpg"
+    my_idea.description = 'my description'
+    my_idea.save!
+
+    visit idea_path(my_idea)
+    fill_in('Comment', with: 'this is my dummy comment')
+    click_on('Post')
+    assert current_path.include?(ideas_path)
+    assert page.has_content('this is my dummy comment')
+  end
+
+end
