c8cdd75359523d999de2962ea9306faf3aaef21e
juerg80 <juerg_mueller@vtxmail.ch>
2020-12-15 20:52:55 +0100

    task 3.3 done

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,7 +1,16 @@
 class AccountController < ApplicationController
+  before_action :ensure_authenticated
 
   def ideas
-    @ideas = Idea.all
+    user_id = session[:user_id]
+    user = User.find(user_id)
+    @ideas = user.goals
+  end
+
+  def ensure_authenticated
+    unless(logged_in?)
+      redirect_to login_path
+    end
   end
 
 end
