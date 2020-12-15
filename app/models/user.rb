22cd17756ff00847fbf5c5ca2cf89378f2008508
juerg80 <juerg_mueller@vtxmail.ch>
2020-12-15 19:56:08 +0100

    task 2.14 done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   has_many :ideas
   has_secure_password
 
+  before_validation :downcase_email
+
   has_and_belongs_to_many :goals, class_name: 'Idea'
 
-  validates :email, uniqueness: true  
+  validates :email, uniqueness: true
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
