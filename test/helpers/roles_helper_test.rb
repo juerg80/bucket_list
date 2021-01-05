2c7c23acaa69019747f9971a5a64e552d326c627
juerg80 <juerg_mueller@vtxmail.ch>
2021-01-05 21:00:59 +0100

    task 3.18: wip

---
+++ test/helpers/roles_helper_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class RolesHelperTest < ActionView::TestCase
+  attr_accessor :current_user
+
+  test 'registered users can edit owned ideas' do
+    owner = User.create(email: 'test@epfl.ch', password: 'password')
+    self.current_user = owner
+
+    # idea = Idea.create(title: 'A test idea!', user: owner)
+    idea = Idea.create(title: 'A test idea!')
+
+    assert can_edit?(idea)
+  end
+end
