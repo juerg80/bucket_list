56283cb369060ddf717e60836f6e46ddd6023ca2
juerg80 <juerg_mueller@vtxmail.ch>
2021-01-10 14:22:39 +0100

    before db mess

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,13 +4,10 @@ class Idea < ApplicationRecord
 
   has_many :comments
   has_and_belongs_to_many :users
-
-  def self.search(search_term)
-    where('title LIKE ?', "%#{search_term}%") | where('description LIKE ?', "%#{search_term}%")
-  end
-
-  def self.most_recent()
-    Idea.all.order(created_at: :desc).limit(3)
-  end
+  
+  scope :most_recent, -> { order(created_at: :desc).limit(3) }
+  scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :description_contains, ->(term) { where('description LIKE ?', "%#{term}%") }
+  scope :search, ->(search_term) { title_contains(search_term).or(description_contains(search_term)) }
 
 end
