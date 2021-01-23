070ef7335755eebaa6cc980a112e06ed624ddfe3
juerg80 <juerg_mueller@vtxmail.ch>
2021-01-23 14:54:11 +0100

    task 7.6: done

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,5 @@
+Unsplash.configure do |config|
+  config.application_access_key   = "Z0Z9ajiSCTB55V6Ene9KKVoxZYHtqx7nAEmOXIaDGXI"
+  config.application_secret = "EtEgtVDlpmEj79ua8FXhpFRfMi4QQpKRo5pO1gxbIv0"
+  config.utm_source         = "exts-fullstack"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :ideas do
     resources :comments
+    resources :photos
   end
 
   get '/account/ideas'
